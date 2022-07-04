74afe56b4e8c0141dacafb0b020a65a019890ebf
Feeld <mezeimate000@gmail.com>
2022-07-04 15:59:57 +0200

    initial import

---
+++ src/test/java/com/mezeim/bucketlistmaker/BucketListMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mezeim.bucketlistmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BucketListMakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
